bf81c76f1832287feda3287ddb7bbfe740ebdb2d
Elian Webster <elianwebster0@gmail.com>
2024-07-09 01:29:01 -0500

    Add Flask app files and configuration for deployment with Vercel

---
+++ app/view.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, send_from_directory
+
+
+app = Flask(__name__, static_folder='static')
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
